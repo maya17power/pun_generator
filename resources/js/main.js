1b51adc5745fd542ef78416069db136d36c27e4d
WildStyle <9sanchez@protonmail.com>
2023-12-16 19:59:14 -0800

    added dom elements to variables on main js file.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,44 +1,50 @@
 
+// window.addEventListener('load',searchPun);
 
 window.onload = function() {
 
-    let punAPI = "https://www.punapi.rest/api/pun";
-  
+    const punAPI = "https://www.punapi.rest/api/pun";
+    const showAnswer = document.querySelector('#showAnswer');
+    const punAnswer = document.querySelector('#punAnswer');
+    const punRefresh = document.querySelector('#punRefresh');
+    const punQuestion = document.querySelector('#punQuestion');
+
     //on show answer button click
-    document.getElementById("showAnswer").onclick = function(){
+    showAnswer.onclick = function(){
       //show the answer div
-      document.getElementById("punAnswer").style.display = "block";
+      punAnswer.style.display = "block";
       //hide the show answer button
-      document.getElementById("showAnswer").style.display = "none";
+      showAnswer.style.display = "none";
     }
     
     //on generate Pun button click 
-    document.getElementById("punRefresh").onclick = function() {
+    punRefresh.onclick = function(){
+    punAnswer.innerHTML = "";
       //hide the answer div
-      document.getElementById("punAnswer").style.display = "none";
-      searchPun();
+    punAnswer.style.display = "none";
+    searchPun();
     };
   
-    function searchPun() {
+    function searchPun(){
       xhr = new XMLHttpRequest();
       xhr.open("GET",punAPI,true);
       xhr.send();
       xhr.onreadystatechange = processPun;
     };
   
-    function processPun() {
+    function processPun(){
       if(xhr.readyState == 4 && xhr.status == 200){
         let myPun = JSON.parse(xhr.responseText);
         let qAnda = punFilter(myPun.pun);
-          document.getElementById("punQuestion").innerHTML =  qAnda[0];
+          punQuestion.innerHTML =  qAnda[0];
           if(qAnda[1] != ""){
-             document.getElementById("showAnswer").style.display = "block";
-             document.getElementById("punAnswer").innerHTML = qAnda[1];
+             showAnswer.style.display = "block";
+             punAnswer.innerHTML = qAnda[1];
           }else{
-             document.getElementById("punAnswer").style.display = "none";
+             punAnswer.style.display = "none";
           }
         }else{
-          document.getElementById("punQuestion").innerHtml = "No pun, search again! :("
+          punQuestion.innerHtml = "No pun, search again! :("
       }
     };
   
