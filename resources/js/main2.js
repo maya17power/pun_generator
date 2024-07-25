5829ad7723806b619508670cefdaa6132cc55be1
maya17power <9sanchez@protonmail.com>
2024-07-25 16:30:29 -0700

    update pun generator program. API is EOL

---
+++ resources/js/main2.js
@@ -0,0 +1,16 @@
+//when user clicks 'punGeneratorButton'
+//generate random number between 1-200.
+
+
+//find number in obj.
+
+
+//get pun and answer from obj matching random number.
+
+
+//display pun but hide the answer.
+
+
+//click the button 'give up' to display answer.
+
+
